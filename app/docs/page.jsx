3d4d2b3092cac25aa2272429693eb496f4c91296
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2023-10-03 16:21:48 +0630

    change-text

--- app/docs/page.jsx
+++ app/docs/page.jsx
@@ -9,15 +9,15 @@ function DocsPage() {
         <h1 className=" text-lg md:text-xl font-medium">Description</h1>
         <p className=" mt-3 flex flex-col gap-2">
           <span className=" text-gray-500">
-            Destiny and love are like two best friends who sometimes surprise
+            {`Destiny and love are like two best friends who sometimes surprise
             us. Destiny is like a map of our life's journey that we can't
             change. Love, on the other hand, is a strong feeling that can change
-            the way we follow that map.
+            the way we follow that map.`}
           </span>
           <span className=" text-gray-500">
-            Imagine you meet someone special by chance, and you fall in love.
+            {`Imagine you meet someone special by chance, and you fall in love.
             That's destiny and love working together. Love can make us do things
-            we never expected. It can take us on adventures we never planned.
+            we never expected. It can take us on adventures we never planned.`}
           </span>
           <span className=" text-gray-500">
             So, destiny sets the path, and love adds exciting twists and turns
